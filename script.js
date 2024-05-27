3dd9939c49d98caba5e174f03ef515f89c188278
thivyadeekzan <thivyadeekzanmanievannan@gmail.com>
2024-05-28 03:23:05 +0530

    Finished my Portfolio Website

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".snallscreen-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
